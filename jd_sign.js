1511a6d09e320e027af14736617cd91898670602
jackqwc <66513765+jackqwc@users.noreply.github.com>
2021-02-11 10:07:18 +0800

    Update jd_sign.js

--- jd_sign.js
+++ jd_sign.js
@@ -64,7 +64,7 @@ function sendNotificationIfNeed() {
   let SCKEY = push_key.replace(/[\r\n]/g,"")
 
   const options ={
-    uri:  `https://sc.ftqq.com/${SCKEY}.send`,
+    uri:  ` https://sctapi.ftqq.com/SCT6126T0qnMWwQe2uXlHTjbp5rkOaDT.send`,
     form: { text, desp },
     json: true,
     method: 'POST'
